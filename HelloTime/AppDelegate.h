41a56b7b42bf97695a3fc5d16aeecb67a7b09ecc
Mark Pleskac <markpleskac@gmail.com>
2014-10-19 18:09:48 -0700

    Initial Commit

---
+++ HelloTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloTime
+//
+//  Created by Mark Pleskac on 10/19/14.
+//  Copyright (c) 2014 Mark Pleskac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloTime
+//
+//  Created by Mark Pleskac on 10/19/14.
+//  Copyright (c) 2014 Mark Pleskac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
