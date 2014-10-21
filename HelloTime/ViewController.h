7123fa47a7f12ef1042574b07865102efd498ca0
Mark Pleskac <markpleskac@gmail.com>
2014-10-20 17:51:40 -0700

    Added time label

--- HelloTime/ViewController.h
+++ HelloTime/ViewController.h
@@ -9,6 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *timeLabel;
+@property (strong, nonatomic) IBOutlet UILabel *timeLabel2;
 
 
 @end
--- HelloTime/ViewController.m
+++ HelloTime/ViewController.m
@@ -17,11 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self checkTime:self];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(void)checkTime:(id)sender{
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    [formatter setDateFormat:@"hh:mm:ss"];
+    [self.timeLabel2 setText:[formatter stringFromDate:[NSDate date]]];
+    
+    //recurse
+    [self performSelector:@selector(checkTime:) withObject:self afterDelay:1.0];
 }
 
 @end
